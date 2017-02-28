c6a3c2518bfcce6f31ccbb85043fc22e76693c56
janusz jas <janusz@pietkun.pl>
2017-02-28 11:26:08 +0100

    added few changes ( second viewController and buttons on it)

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by janusz jas on 28.02.2017.
+//  Copyright © 2017 Janusz Pietkun. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    //record jako outlet, by zmieniał sie na stop
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+    
+}
